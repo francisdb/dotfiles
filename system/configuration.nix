8a31a1f3090a2aa30789731987a970f627e650c6
Francis De Brabandere <francisdb@gmail.com>
2025-06-18 20:24:57 +0200

    nixos: gcc for rust

--- system/configuration.nix
+++ system/configuration.nix
@@ -166,6 +166,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     rustup
+    gcc
     git
     gnupg
     spotify
